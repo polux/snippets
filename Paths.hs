4e0aa64ff97d5cad66a8419b23575a743ef26949
Paul Brauner <polux@google.com>
2016-05-25 14:17:37 +0200

    paths from leaves to root

---
+++ Paths.hs
@@ -0,0 +1,37 @@
+-- Copyright 2016 Google Inc. All Rights Reserved.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+import Data.Tree
+
+type Path a = [a]
+
+paths :: Tree a -> [Path a]
+paths tree = paths' [] tree
+
+paths' :: Path a -> Tree a -> [Path a]
+paths' prefix (Node x []) = [x:prefix]
+paths' prefix (Node x children) = concatMap (paths' (x:prefix)) children
+
+tree =
+  Node "a" [
+    Node "b" [
+      Node "c" [],
+      Node "d" []
+    ],
+    Node "e" [
+      Node "f" []
+    ]
+  ]
+
+main = print (paths tree)
