a577cfa8258c3886aa500a67fbc0a7c5efd4e349
Paul Brauner <polux@google.com>
2019-12-20 15:51:26 +0100

    remove unnecessary language pragma

--- Assignment.hs
+++ Assignment.hs
@@ -15,8 +15,6 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-{-# LANGUAGE TupleSections #-}
-
 import Data.Function
 import Data.List
 import System.Random
